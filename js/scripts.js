77c5481ddd5f993548d6d9a7e2f574836ab490f9
Dj2035 <dk.jude@yahoo.com>
2022-03-30 11:43:36 +0200

    Update scripts.js
    Added the variable pokemonList with an array of 4 objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'Jealof Rice';
-document.write(favoriteFood);
+let pokemonList = [
+	{name: "Balbasaur", height: 7, types: ["grass", "poison"], weaknesses: ["fire", "psychic", "flying", "ice"]},
+	{name: "Metapod", height: 7, types: "bug", weaknesses: ["fire", "flying", "rock"]},
+	{name: "Charmeleon", height: 11, types: ["monster", "dragon"], weaknesses: ["water", "ground", "rock"]},
+	{name: "Pikachu", height: 4, types: ["field", "fairy"]}
+];
