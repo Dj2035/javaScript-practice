b685e8f6cd96b6073ae65d89c185adfe0a4c2fde
Dj2035 <dk.jude@yahoo.com>
2022-04-29 15:44:47 +0200

    Add 2 pokemon to pokemon list and use the IIFE functions to access pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,85 @@
+// IIFE
+let pokemonRepository = (function () {
+	// List is pokemons
+	let pokemonList = [
+		{
+			name: "Balbasaur",
+			height: 7,
+			types: ["grass", "poison"],
+			weaknesses: ["fire", "psychic", "flying", "ice"]
+		},
+		{
+			name: "Metapod",
+			height: 7,
+			types: ["bug"],
+			weaknesses: ["fire", "flying", "rock"]
+		},
+		{
+			name: "Charmeleon",
+			height: 11,
+			types: ["monster", "dragon"],
+			weaknesses: ["water", "ground", "rock"]
+		},
+		{
+			name: "Pikachu",
+			height: 4,
+			types: ["field", "fairy"],
+			weaknesses: ["ground"]
+		}
+	];
 
-// set variables
-let threshold = 10;
+	//Defining add function to add pokemon to list
+	function add(pokemon1, pokemon2) {
+		pokemonList.push(pokemon1, pokemon2);
+	}
+	// adding the typeof function (Needs to be checked)
+	/* function add(pokemon1, pokemon2) {
+			if (typeof pokemon1 && pokemon2 === "object") {
+			pokemonList.push(pokemon1, pokemon2);
+			}else {
+			document.write(This is not an object);
+			}
+		}
+	*/
 
-// List of Pokemons
-let pokemonList = [
+	//Defining getAll function to return pokemonList
+	function getAll() {
+		return pokemonList;
+	}
+
+	// Returning getAll and add functions
+	return {
+		add: add,
+		getAll: getAll
+	};
+})();
+
+// Adding 2 additional pokemons to pokemon list
+pokemonRepository.add(
 	{
-		name: "Balbasaur",
-		height: 7,
-		types: ["grass", "poison"],
-		weaknesses: ["fire", "psychic", "flying", "ice"]
+		name: "Wartortle",
+		height: 3,
+		types: ["water"],
+		weaknesses: ["grass", "electric"]
 	},
 	{
-		name: "Metapod",
-		height: 7,
+		name: "Caterpie",
+		height: 1,
 		types: ["bug"],
 		weaknesses: ["fire", "flying", "rock"]
-	},
-	{
-		name: "Charmeleon",
-		height: 11,
-		types: ["monster", "dragon"],
-		weaknesses: ["water", "ground", "rock"]
-	},
-	{
-		name: "Pikachu",
-		height: 4,
-		types: ["field", "fairy"],
-		weaknesses: ["ground"]
 	}
-];
+)
 
+// Html
 document.write("<h1>Available Pokémons</h1>");
-document.write("<ul>");
-// command to iterate through pokemon list and print it with height info to index.html
-pokemonList.forEach(function(item) {
-    // add comment to pokemons taller than threshold
-    let isBig = item.height > threshold ? "- Wow, that's big!" : "";
+document.write("<ul>")
+
+// forEach loop using function that allows to retrieve the pokemon list
+pokemonRepository.getAll().forEach(function(pokemon) {
+    // condition statement for big pokemon
+    let isBig = pokemon.height > 10 ? "- Wow, that's big!" : "";
 	document.write(
-        `<li><b>${item.name}</b><br> &nbsp&nbsp(height: ${item.height}) ${isBig} </li>`
+        `<li><b>${pokemon.name}</b><br> &nbsp&nbsp(height: ${pokemon.height}) ${isBig} </li>`
    );
 })
-document.write("</ul>");
+document.write("</ul>")
